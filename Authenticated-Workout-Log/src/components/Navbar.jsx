0078aa54dd5649ba432773278bbfe3e4051d6ffa
zypherson <taylorzypherson@gmail.com>
2025-08-09 11:38:28 -0400

    updated nav bar

--- Authenticated-Workout-Log/src/components/Navbar.jsx
+++ Authenticated-Workout-Log/src/components/Navbar.jsx
@@ -1,9 +1,22 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
 const Navbar = () => {
-    return (
-      <nav className="p-4 bg-gray-800 text-white flex justify-between">
-        <h1>Workout Log</h1>
-      </nav>
-    );
-  };
-  
-  export default Navbar;
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
+  return (
+    <nav className="p-4 bg-gray-800 text-white flex justify-between">
+      <h1>Workout Log</h1>
+      {isAuthenticated ? (
+        <button onClick={() => logout({ returnTo: window.location.origin })}>
+          Logout
+        </button>
+      ) : (
+        <button onClick={() => loginWithRedirect()}>
+          Login
+        </button>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
