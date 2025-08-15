1b29d3f38d41a037df25bab30bf6aa8d140bc024
zypherson <taylorzypherson@gmail.com>
2025-08-15 11:09:14 -0400

    fleshed out home page

---
+++ Authenticated-Workout-Log/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div className="p-6">
+        <h1 className="text-3xl font-bold">Welcome to Workout Log</h1>
+        <p>Track your workouts and stay consistent 💪</p>
+      </div>
+    );
+  };
+  
+  export default Home;
