f58ebab8306ff8ac46cfa3a3281196fcfbec8789
zypherson <taylorzypherson@gmail.com>
2025-08-16 11:20:34 -0400

    added auth0 functioality to login page

--- Authenticated-Workout-Log/src/pages/Login.jsx
+++ Authenticated-Workout-Log/src/pages/Login.jsx
@@ -1,9 +1,17 @@
-const Login = () => {
-    return (
-      <div className="p-6">
-        <h1 className="text-3xl font-bold">Login Page</h1>
-      </div>
-    );
-  };
-  
-  export default Login;
+import { useAuth0 } from "@auth0/auth0-react";
+
+export default function Login() {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Login</h1>
+      <button
+        onClick={() => loginWithRedirect()}
+        className="bg-blue-500 px-4 py-2 rounded text-white"
+      >
+        Login with Auth0
+      </button>
+    </div>
+  );
+}
