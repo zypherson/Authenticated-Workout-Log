7a7a324210612f114c95886996362c4a537813d0
zypherson <taylorzypherson@gmail.com>
2025-08-18 15:12:12 -0400

    added role requirements in pivate route

--- Authenticated-Workout-Log/src/components/PrivateRoute.jsx
+++ Authenticated-Workout-Log/src/components/PrivateRoute.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable react-refresh/only-export-components */
 import { useAuth0 } from '@auth0/auth0-react';
 import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }) => {
-  const { isAuthenticated, isLoading } = useAuth0();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  return isAuthenticated ? children : <Navigate to="/" />;
-};
-
+const PrivateRoute = ({ children, requiredRole }) => {
+    const { user, isAuthenticated, isLoading } = useAuth0();
+  
+    if (isLoading) return <div>Loading...</div>;
+  
+    const hasRole = user?.['https://your-app.com/roles']?.includes(requiredRole);
+  
+    return (isAuthenticated && hasRole) 
+      ? children 
+      : <Navigate to="/" />;
+  };
+  
 export default PrivateRoute;
